dd9d22c958dab903d33467c9c35d11b9d84cbf8d
GitHub Action <action@github.com>
2021-11-30 09:33:02 +0000

    Template cleanup

--- src/main/kotlin/com/github/shebangdog/onewaygenerator/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/shebangdog/onewaygenerator/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.shebangdog.onewaygenerator.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.shebangdog.onewaygenerator.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/shebangdog/onewaygenerator/services/MyProjectService.kt
+++ src/main/kotlin/com/github/shebangdog/onewaygenerator/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.shebangdog.onewaygenerator.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.shebangdog.onewaygenerator.MyBundle
 
 class MyProjectService(project: Project) {
 
