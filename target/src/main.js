1335b3e84c9cd8f87128aed2996df2286208f320
ShebangDog <twilight.sky.asterisk@gmail.com>
2021-12-26 20:17:21 +0900

    save

--- target/src/main.js
+++ target/src/main.js
@@ -14,17 +14,10 @@ const handleChange = (event) => {
 
 const bindInput = <input value={value} onChange={handleChange} />
 
-const [value1, setValue1] = useState(value)
+const [value1, setValue1] = useState(initialValue)
 const handleChange1 = (event) => {
   setValue1(event.target.value1)
 }
 
 const bindInput1 = <input value={value1} onChange={handleChange1} />
-
-const [value2, setValue2] = useState(initialValue)
-const handleChange2 = (event) => {
-  setValue2(event.target.value2)
-}
-
-const bindInput2 = <input value={value2} onChange={handleChange2} />
 
